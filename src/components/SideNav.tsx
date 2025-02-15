14e484472aaaeb9a800b85f95bc92122ec74da82
R Singireddy <rpsingireddy@gmail.com>
2025-02-15 15:31:49 -0600

    Update SideNav.tsx

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -45,7 +45,7 @@ const SideNav = () => {
             <img
               src="/lovable-uploads/cb9a4c3e-89f7-4f9f-9cc2-9c506bc7be70.png"
               alt="Company Logo"
-              className="h-14 w-14 brightness-110"
+              className="h-15 w-15 brightness-110"
             />
           </Link>
         </div>
