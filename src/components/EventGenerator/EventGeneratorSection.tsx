64514816a9c4545f1d039a719f70607e132e169b
R Singireddy <rpsingireddy@gmail.com>
2025-02-19 17:07:31 -0600

    Update EventGeneratorSection.tsx

--- src/components/EventGenerator/EventGeneratorSection.tsx
+++ src/components/EventGenerator/EventGeneratorSection.tsx
@@ -66,7 +66,7 @@ export const EventGeneratorSection = () => {
         <div className="flex justify-center gap-4">
           <Button
             onClick={() => navigate("/discover")}
-            className="animate-fade-up gap-2 bg-[#B9EBFF] hover:bg-[#33C3F0]/90"
+            className="animate-fade-up gap-2 bg-[#B9EBFF] hover:bg-[#B9EBFF]/90"
             size="lg"
           >
             Explore Events
